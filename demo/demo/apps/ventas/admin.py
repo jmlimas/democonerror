b1fc71bbee06386f4f349db78dc29ba7d7c7f2d7
Juan Manuel Limas <jmlimas@gmail.com>
2013-01-26 11:48:14 -0600

    Revert "nuevo commit"
    This reverts commit 478aadad724e50fc42c5669dcfa40733aa637e93.

--- demo/demo/apps/ventas/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from demo.apps.ventas.models import cliente,producto
-
-# Registramos  los  modelos de ventas
-admin.site.register(cliente)
-admin.site.register(producto)
--- demo/demo/apps/ventas/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class addProductForm(forms.form):
-	nombre = forms.CharField(widget= forms.TextInput())
-	descripcion = forms.CharField(widget= forms.TextInput())
-
-	def clean(self):
-		return self.cleaned_data	
